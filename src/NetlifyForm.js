3e4341d1f6b7d7b46ab7429c42cf39dcd513db72
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-04-14 12:49:44 +0900

    change form name

--- src/NetlifyForm.js
+++ src/NetlifyForm.js
@@ -37,7 +37,7 @@ class NetlifyForm extends React.Component {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "inqform", ...this.state })
     })
       .then(() => {
         this.setState({
