73ed21b1768229d939e253a39026f34c399c914d
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-04-24 19:39:54 +0900

    add plugin for eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: "gatsby-plugin-eslint-v2",
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
 };
