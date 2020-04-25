8e777e3683f705ee746fc2cd2bd59891eb9b873b
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-04-25 12:01:40 +0900

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,19 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `tombo gokuraku portfolio`,
+        short_name: `Tombo`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/logo512.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 };
