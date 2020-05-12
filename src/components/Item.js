6b000915660f736f1decf9ff86a18533c5c3d2ab
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-05-12 16:58:17 +0900

    adjust Item margin font-size

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,27 +3,28 @@ import breaks from "../style/styled-breaks";
 
 const Item = styled.div`
   flex-basis: ${(props) => (props.main ? "100%" : "auto")};
-  font-size: ${(props) => (props.main ? "1.4" : "1.2")}rem;
+  font-size: ${(props) => (props.main ? "1.2" : "1.0")}rem;
+  font-weight: ${(props) => (props.main ? "bold" : "normal")};
   color: ${(props) => props.theme.primaryLight};
   border: solid 2px ${(props) => props.theme.primary};
   border-radius: 8px;
   text-align: center;
   padding: 4px 8px;
-  margin: 4px;
+  margin: 2px;
 
   ${breaks.greaterThan("sm")`
-    font-size: ${(props) => (props.main ? "2" : "1.8")}rem;
-    margin: 12px 16px;
+    font-size: ${(props) => (props.main ? "1.6" : "1.4")}rem;
+    margin: 8px 12px;
   `}
 
   ${breaks.greaterThan("md")`
-    font-size: ${(props) => (props.main ? "2.2" : "2")}rem;
-    margin: 16px 20px;
+    font-size: ${(props) => (props.main ? "2.0" : "1.8")}rem;
+    margin: 12px 16px;
   `}
 
   ${breaks.greaterThan("lg")`
     font-size: ${(props) => (props.main ? "2.4" : "2.2")}rem;
-    margin: 20px 24px;
+    margin: 16px 20px;
   `}
 
 `;
